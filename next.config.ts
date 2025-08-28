d0f54edc959d6d7ecf735b8ae9d8c7736e11939c
stevenmulya <stevenmulya@gmail.com>
2025-08-28 16:06:07 +0700

    com

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+// next.config.ts
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ihjgyjucrukxybcnvyxf.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
