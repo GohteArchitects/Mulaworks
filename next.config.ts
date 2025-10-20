e2f74e66f8fb8343c3c66d2c0e143980d5b275fe
stevenmulya <stevenmulya@gmail.com>
2025-10-20 15:04:20 +0700

    Initial commit: Project setup and all features completed

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      // Konfigurasi yang sudah ada untuk Supabase (JANGAN DIHAPUS)
       {
         protocol: 'https',
         hostname: 'ihjgyjucrukxybcnvyxf.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
-      },
-      // 👇 TAMBAHKAN BLOK INI untuk ImageBB 👇
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
